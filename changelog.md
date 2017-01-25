356e4b7fb7408ce694e95ebb4fea5bf27b88f3c1
Wu Sun <geoalchimista@users.noreply.github.com>
2017-01-25 01:25:26 -0800

    Refined saturation vapor pressure and dew temperature functions. Rename general_func.py to common_func.py

---
+++ changelog.md
@@ -0,0 +1,21 @@
+# Change Log
+
+# 0.1.1 (2017-01-18)
+
+- Added a new chamber lookup table function controlled by external config file `chamber.yaml`.
+- Bug fix in chamber schedule.
+- Added flow data settings to the config.
+- Use `dict.update()` method for user custom config.
+- Variable fix: standard error of flux estimate, `sd_flux_*` --> `se_flux_*`
+
+# 0.1.0 (2017-01-07)
+
+- Main program reorganized into functions.
+- Configuration file generated.
+- Reformatted to comply with PEP8 standard.
+- Bug fix: year number in `flux_calc.flux_calc()`.
+- Added a procedure to generate curve fitting plots.
+
+# 0.0.1 (2016-07-18)
+
+- Created by Wu Sun @ UCLA (wu.sun@ucla.edu).
--- common_func.py
+++ common_func.py
@@ -1,30 +1,26 @@
-#!usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Common functions used in flux calculation
 
-Revision history
-----------------
-- Created by Wu Sun @ UCLA <wu.sun@ucla.edu> (18 July 2016).
-- Restructured. (W.S., 6 Jan 2016)
-- `chamber_lookup_table_func` now generates a function from the chamber
-  schedule configuration `chamber_schedule.yaml`.
+(c) Wu Sun <wu.sun@ucla.edu> 2016-2017
+
 """
 import numpy as np
 import pandas as pd
 from scipy import optimize
-import datetime
+import scipy.constants.constants as sci_const
+# import datetime
 import warnings
-
 from collections import namedtuple
-
 import yaml
+
+
+T_0 = sci_const.zero_Celsius
 
 
 def resistant_mean(x, IQR_range=1.5):
     """
     Input: 1D `numpy.ndarray` like
-    Output: array mean with outliers removed
+    Output: mean with outliers removed
     """
     x = np.array(x)
     if np.sum(np.isfinite(x)) <= 1:
@@ -41,7 +37,7 @@ def resistant_mean(x, IQR_range=1.5):
 def resistant_std(x, IQR_range=1.5):
     """
     Input: 1D `numpy.ndarray` like
-    Output: array standard deviation (degree of freedom == 1) with outliers removed
+    Output: standard deviation (deg. freedom = 1) with outliers removed
     """
     x = np.array(x)
     if np.sum(np.isfinite(x)) <= 1:
@@ -367,19 +363,25 @@ def IQR_func(x):
         return(np.nan)
 
 
+
+
+
+
 def p_sat_h2o(temp, ice=False, kelvin=False, method='gg'):
     """
     Calculate saturation vapor pressure over water or ice at a temperature.
 
+    by Wu Sun <wu.sun@ucla.edu>, 14 Sep 2014
+
     Parameters
     ----------
-    temp : float or `numpy.ndarray`
+    temp : float or array_like
         Temperature, in Celsius degree by default.
-    ice : boolean, optional
+    ice : bool, optional
         Calculate saturation vapor pressure on ice if enabled.
-    kelvin : boolean, optional
+    kelvin : bool, optional
         Temperature input is in Kelvin if enabled.
-    method : string, optional
+    method : str, optional
         Method used to evaluate saturation vapor pressure.
         'gg': default, Goff-Gratch equation (1946). [GG46]_
         'buck': Buck Research Instruments L.L.C. (1996). [B96]_
@@ -387,7 +389,7 @@ def p_sat_h2o(temp, ice=False, kelvin=False, method='gg'):
 
     Returns
     -------
-    e_sat : float or `numpy.ndarray`
+    e_sat : float or array_like
         Saturation vapor pressure in Pascal.
 
     Raises
@@ -397,16 +399,16 @@ def p_sat_h2o(temp, ice=False, kelvin=False, method='gg'):
 
     References
     ----------
-    .. [GG46] Goff, J. A., and Gratch, S. (1946). Low-­pressure properties of 
-              water from -160 to 212 F, in Transactions of the American Society 
-              of Heating and Ventilating Engineers, pp 95-­122, presented at 
-              the 52nd Annual Meeting of the American Society of Heating and 
-              Ventilating Engineers, New York.
-    .. [B96]  Buck Research Instruments L.L.C. (1996). Buck Research CR-1A 
-              User's Manual, Appendix 1.
-    .. [WMO]  World Meteorological Organization. (2008). Guide to 
-              Meteorological Instruments and Methods of Observation, 
-              Appendix 4B, WMO-No. 8 (CIMO Guide), Geneva.
+    .. [GG46] Goff, J. A., and Gratch, S. (1946). Low-­pressure properties of
+       water from -160 to 212 F, in Transactions of the American Society of
+       Heating and Ventilating Engineers, pp 95-­122, presented at the 52nd
+       Annual Meeting of the American Society of Heating and Ventilating
+       Engineers, New York.
+    .. [B96] Buck Research Instruments L.L.C. (1996). *Buck Research CR-1A
+       User's Manual*, Appendix 1.
+    .. [WMO] World Meteorological Organization. (2008). *Guide to
+       Meteorological Instruments and Methods of Observation*, Appendix 4B,
+       WMO-No. 8 (CIMO Guide), Geneva.
 
     Examples
     --------
@@ -425,61 +427,104 @@ def p_sat_h2o(temp, ice=False, kelvin=False, method='gg'):
     >>> print(p_sat_h2o(-15, ice=True))
     165.014773924
 
-    >>> print(p_sat_h2o(273.15, kelvin=True, ice=True, method='cimo'))
+    >>> print(p_sat_h2o(258.15, kelvin=True, ice=True, method='cimo'))
     165.287132017
+
     """
-    
-    T_k = np.array(temp, dtype='d') + (not kelvin) * 273.15
+    T_k = np.array(temp, dtype='d') + (not kelvin) * T_0
     # force temperature to be in Kelvin
-    # pressure = np.array(pressure, dtype='d')
 
     if (np.sum(T_k > 273.16) and ice):
         # The triple point of water is 273.16 K
-        raise ValueError('Temperature error, no ice exists. ')
+        raise ValueError('Temperature error, no ice exists.')
 
-    if (not ice):
-        # equations for liquid water
+    if not ice:
         if method == 'buck':
-            T_c = T_k - 273.15      # temperature in Celsius degree
-            e_sat = 6.1121 * np.exp( (18.678 - T_c / 234.5) * 
-                T_c / (257.14 + T_c) ) * 100
+            T_c = T_k - T_0  # temperature in Celsius degree
+            e_sat = 6.1121 * np.exp((18.678 - T_c / 234.5) *
+                                    T_c / (257.14 + T_c)) * 100
         elif method == 'cimo':
-            T_c = T_k - 273.15      # temperature in Celsius degree
-            e_sat = 6.112 * np.exp( 17.62 * T_c / (243.12 + T_c) ) * 100
+            T_c = T_k - T_0  # temperature in Celsius degree
+            e_sat = 6.112 * np.exp(17.62 * T_c / (243.12 + T_c)) * 100
         else:
             # Goff-Gratch equation by default
-            u_T = 373.16/T_k
-            v_T = T_k/373.16
-            e_sat = ( - 7.90298 * (u_T - 1) + 5.02808 * np.log10(u_T)
-                - 1.3816e-7 * (10**(11.344 * (1 - v_T)) - 1)
-                + 8.1328e-3 * (10**(- 3.49149 * (u_T - 1)) - 1) 
-                + np.log10(1013.246) )
-            e_sat = 10**e_sat * 100
+            u_T = 373.16 / T_k
+            v_T = T_k / 373.16
+            e_sat = (- 7.90298 * (u_T - 1) + 5.02808 * np.log10(u_T) -
+                     1.3816e-7 * (10 ** (11.344 * (1 - v_T)) - 1) +
+                     8.1328e-3 * (10 ** (- 3.49149 * (u_T - 1)) - 1) +
+                     np.log10(1013.246))
+            e_sat = 10 ** e_sat * 100
     else:
-        # equations for ice
         if method == 'buck':
-            T_c = T_k - 273.15      # temperature in Celsius degree
-            e_sat = 6.1115 * np.exp( (23.036 - T_c / 333.7) * 
-                T_c / (279.82 + T_c) ) * 100
+            T_c = T_k - T_0  # temperature in Celsius degree
+            e_sat = 6.1115 * np.exp((23.036 - T_c / 333.7) *
+                                    T_c / (279.82 + T_c)) * 100
         elif method == 'cimo':
-            T_c = T_k - 273.15      # temperature in Celsius degree
-            e_sat = 6.112 * np.exp( 22.46 * T_c / (272.62 + T_c) ) * 100
+            T_c = T_k - T_0  # temperature in Celsius degree
+            e_sat = 6.112 * np.exp(22.46 * T_c / (272.62 + T_c)) * 100
         else:
             # Goff-Gratch equation by default
-            u_T = 273.16/T_k
-            v_T = T_k/273.16
-            e_sat = ( - 9.09718 * (u_T - 1) - 3.56654 * np.log10(u_T) 
-              + 0.876793 * (1 - v_T) + np.log10(6.1071) )
-            e_sat = 10**e_sat * 100
+            u_T = 273.16 / T_k
+            v_T = T_k / 273.16
+            e_sat = (- 9.09718 * (u_T - 1) - 3.56654 * np.log10(u_T) +
+                     0.876793 * (1 - v_T) + np.log10(6.1071))
+            e_sat = 10 ** e_sat * 100
 
     return e_sat
 
-def dew_temp(vap_pres):
-    """
-    Calculate dew temperature from water concentration. 
 
-    Takes only one value as input, does not take arrays.
-    """ 
-    def __dew_temp_func(T, e_sat):
-        return(p_sat_h2o(T) - e_sat)
-    return optimize.newton(__dew_temp_func, x0=25, args=(vap_pres,))
+def dew_temp(e_sat, guess=25., kelvin=False, method='gg'):
+    """
+    Calculate dew temperature from water concentration.
+
+    Parameters
+    ----------
+    e_sat : float
+        Saturation vapor pressure in Pascal. Takes only a single value,
+        no array allowed.
+    guess : float, optional
+        An initial guess for the dew temperature to infer.
+    kelvin : bool, optional
+        Dew temperature calculated is in Kelvin if enabled.
+    method : str, optional
+        Method used to evaluate saturation vapor pressure.
+        'gg': default, Goff-Gratch equation (1946). [GG46]_
+        'buck': Buck Research Instruments L.L.C. (1996). [B96]_
+        'cimo': CIMO Guide (2008). [WMO]_
+
+    Returns
+    -------
+    T_dew : float
+        Dew temperature.
+
+    Raises
+    ------
+    AssertionError
+        If the argument `e_sat` is not float or int.
+
+    Examples
+    --------
+    >>> dew_temp(3165)
+    24.998963153421172
+
+    >>> dew_temp(610)
+    -0.0075798295337097081
+
+    >>> dew_temp(3165, kelvin=True)
+    298.14896315342116
+
+    """
+    def __e_sat_residual(T, e_sat, kelvin, method):
+        return(p_sat_h2o(T, kelvin=kelvin, method=method) - e_sat)
+
+    assert type(e_sat) is float or type(e_sat) is int, \
+        "Takes a single value as the argument, array not allowed."
+
+    if kelvin:
+        guess += T_0
+
+    T_dew = optimize.newton(__e_sat_residual, x0=guess,
+                            args=(e_sat, kelvin, method))
+
+    return T_dew
