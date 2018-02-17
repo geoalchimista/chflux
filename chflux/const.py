0d6ef04401e3f22183d684ef02d15b08fce46fae
Wu Sun <geoalchimista@users.noreply.github.com>
2018-02-17 02:04:04 -0800

    MAINT: moved constants

---
+++ chflux/const.py
@@ -0,0 +1,33 @@
+"""
+===============================
+Constants (:mod:`chflux.const`)
+===============================
+
+.. currentmodule:: chflux.const
+
+Physical constants.
+
+====================  =========================================================
+``T_0``               zero Celsius in Kelvin
+``p_std``             standard atmospheric pressure [Pa]
+``R``                 molar gas constant [J mol\ :sup:`-1`\ K\ :sup:`-1`\ ]
+``air_conc_stp``      air concentration at STP condition [mol m\ :sup:`-3`\ ]
+====================  =========================================================
+"""
+from scipy.constants import constants as _constants
+
+
+__all__ = ['T_0', 'p_std', 'R_gas', 'air_conc_stp']
+
+
+# zero Celsius in Kelvin
+T_0 = _constants.zero_Celsius
+
+# standard atmospheric pressure [Pa]
+p_std = _constants.atm
+
+# molar gas constant [J mol^-1 K^-1]
+R_gas = _constants.R
+
+# air concentration at STP condition [mol m^-3]
+air_conc_stp = _constants.atm / (_constants.R * _constants.zero_Celsius)
