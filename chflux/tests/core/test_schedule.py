b64ad2a7fdc0d9a2eb221bd94214d29233d2dca9
Wu Sun <37353134+wusunlab@users.noreply.github.com>
2018-07-25 15:23:17 -0700

    TST: unit tests for core.schedule.label_chamber_period

--- chflux/tests/core/test_schedule.py
+++ chflux/tests/core/test_schedule.py
@@ -1,6 +1,7 @@
 import json
 import math
 
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -20,7 +21,7 @@ def test_time_unit_conversion_factor():
     for unitname in ["d", "day", "days"]:
         assert math.isclose(1.0,
                             schedule.time_unit_conversion_factor(unitname))
-    # edge test: no conversion for illegal string or input types
+    # null cases: no conversion for illegal string or input types
     assert math.isclose(1.0, schedule.time_unit_conversion_factor("null"))
     assert math.isclose(1.0, schedule.time_unit_conversion_factor(42))
 
@@ -62,5 +63,46 @@ def test_make_daily_schedule():
             schdf3 = schedule.make_daily_schedule(ts3, chamber_spec_json)
 
 
-def test_label_chamber_period():  # TODO
-    pass
+def test_label_chamber_period():
+    label = "bm.index"
+    df_bm = pd.read_csv(
+        "./chflux/tests/_assets/test-input-biomet.csv", parse_dates=[0, ])
+    # add timedelta
+    df_bm["timedelta"] = (df_bm["timestamp"] -
+                          df_bm.loc[0, "timestamp"].floor("D")) / \
+        pd.Timedelta(1, "D")
+    # make a copy for mutation test
+    df_bm_cpy = df_bm.copy()
+
+    with open("./chflux/tests/_assets/test-chamber.json") as f:
+        chamber_spec_json = json.load(f)
+
+    ts = pd.Timestamp("2013-04-01 00:00")
+    sch = schedule.make_daily_schedule(ts, chamber_spec_json)
+
+    df_bm_labeled = schedule.label_chamber_period(
+        df_bm, sch, start="schedule.start", end="schedule.end", label=label)
+
+    # the function must be immutable if inplace=False
+    assert df_bm.equals(df_bm_cpy)
+    assert df_bm_labeled["bm.index"].dtype == np.dtype('int64')
+
+    # test the case when chamber_id does not match
+    df_bm_bad_id = df_bm.copy()
+    df_bm_bad_id["chamber_id"] += 20
+    df_bm_bad_id_labeled = schedule.label_chamber_period(
+        df_bm_bad_id, sch, start="schedule.start", end="schedule.end",
+        label=label)
+    assert all(df_bm_bad_id_labeled["bm.index"] == -1)
+
+    df_bm_bad_id_ignored_labeled = schedule.label_chamber_period(
+        df_bm_bad_id, sch, start="schedule.start", end="schedule.end",
+        label=label, match_chamber_id=False)
+    assert all(df_bm_bad_id_ignored_labeled["bm.index"] == -1) is False
+
+    # test inplace=True mutation
+    assert schedule.label_chamber_period(
+        df_bm, sch, start="schedule.start", end="schedule.end", label=label,
+        inplace=True) is None
+    assert df_bm.equals(df_bm_cpy) is False
+    assert label in df_bm.columns
