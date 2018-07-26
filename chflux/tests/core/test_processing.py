b766e2bebacb1c9f0a4d3d947586cddbdf1f4fb2
Wu Sun <37353134+wusunlab@users.noreply.github.com>
2018-07-25 17:16:46 -0700

    added tests for core.processing.reduce_dataframe

--- chflux/tests/core/test_processing.py
+++ chflux/tests/core/test_processing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from chflux.core import processing
+import json
+from chflux.core import processing, schedule
 
 
 def test_find_timestamp():
@@ -44,3 +45,37 @@ def test_find_biomet_vars():
     assert isinstance(null_biomet_vars, dict)
     for _, v in null_biomet_vars.items():
         assert v == []
+
+
+def test_reduce_dataframe():
+    label = "bm.index"
+    df_bm = pd.read_csv(
+        "./chflux/tests/_assets/test-input-biomet.csv", parse_dates=[0, ])
+
+    ts_name = processing.find_timestamp(df_bm)
+    # add timedelta
+    df_bm["timedelta"] = (df_bm[ts_name] -
+                          df_bm.loc[0, ts_name].floor("D")) / \
+        pd.Timedelta(1, "D")
+
+    with open("./chflux/tests/_assets/test-chamber.json") as f:
+        chamber_spec_json = json.load(f)
+    date = df_bm.loc[df_bm.shape[0] // 2, ts_name].floor("D")
+    sch = schedule.make_daily_schedule(date, chamber_spec_json)
+
+    df_bm_labeled = schedule.label_chamber_period(
+        df_bm, sch, start="schedule.start", end="schedule.end", label=label)
+    df_bm_reduced = processing.reduce_dataframe(
+        df_bm_labeled, by=label, lower_bound=-0.5)
+    df_bm_reduced = df_bm_reduced.drop(columns=["timedelta", "chamber_id"])
+    df_bm_reduced = df_bm_reduced.set_index(label, drop=True)
+    df_bm_processed = sch[["id", "name", "area", "volume"]]
+    df_bm_processed = df_bm_processed.join(df_bm_reduced)
+
+    assert label not in df_bm_processed.columns
+    assert "timedelta" not in df_bm_processed.columns
+
+    biomet_vars = processing.find_biomet_vars(df_bm)
+    for _, namelist in biomet_vars.items():
+        for name in namelist:
+            assert name in df_bm_processed.columns
